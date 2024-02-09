144eda841625ab0456fcc95695bb8353ebef8a32
JonathanFlores444 <jonathan.flores289@ist17dejulio.edu.ec>
2024-02-09 18:09:54 -0500

    commit inicial

---
+++ CONFIG/global.php
@@ -0,0 +1,12 @@
+<?php
+//ip de ka oc servidor de la vase de datos
+define("DB_HOST", "localhost");
+//nombre de la base de datos
+define("DB_NAME", "hacienda");
+//nombre de usuario de la bdd
+define("DB_USERNAME","root");
+//contraseña de usuario de la bdd
+define("DS_PASSWORD","jflores.2023");
+//Codificacion de caracteres
+define("DB_ENCODE","uft8");
+?>
